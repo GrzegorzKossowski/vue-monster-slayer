097e3926fab5c0420ea7e64a2aa7ff64b816321f
Grzegorz Kossowski <grzegorz.kossowski@gmail.com>
2020-09-24 00:15:48 +0200

    deploy.sh updated

--- deploy.sh
+++ deploy.sh
@@ -3,18 +3,26 @@
 # abort on errors
 set -e
 
+# wpisz message dla gita
+read -p "Enter git message : " gitmsg
+
 # build
 npm run build
 
 # navigate into the build output directory
 cd dist
+echo '# vue-monster-slayer
+Simple Vue monster slayer application.
+See [online version](https://grzegorzkossowski.github.io/vue-monster-slayer/).
+' > README.md
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
 git init
 git add -A
-git commit -m 'deploy'
+#git commit -m 'deploy'
+git commit -m "$gitmsg"
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
